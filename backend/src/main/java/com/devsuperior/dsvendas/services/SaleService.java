f6beb222548c3d16233c34181407203d117f63fc
Mikael Tavares <mikael.tavares@unigranrio.br>
2021-05-06 23:24:25 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsvendas.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	//Busca paginada de vendas
+	@Autowired
+	private SaleRepository saleRepository;
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true) 
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll(); //Resolvendo problema de fazer uma consulta pra cada vendedor, deixando na memória os 5 com uma consulta.
+		//Criando uma variável do tipo lista de sale para receber o resultado da busca no banco.
+		Page<Sale> result = saleRepository.findAll(pageable); //O repository tem um método findall que recebe um pageable
+		//O map converte uma coleção para uma nova coleção que pode ser de outro tipo. Neste caso
+		//vai ser uma nova lista de saleDTO. Então será convertido o tipo sale para saleDTO.
+		return result.map(x -> new SaleDTO(x));
+	}
+}
